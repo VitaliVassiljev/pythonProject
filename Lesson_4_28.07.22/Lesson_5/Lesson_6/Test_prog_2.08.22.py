ac198a97f3ac7ab73adee378b02c1727b4dcc05f
MashaChe1 <maria.chebotareva@gmail.com>
2022-08-03 16:15:14 +0300

    Vitali test 1,2,4

---
+++ Lesson_4_28.07.22/Lesson_5/Lesson_6/Test_prog_2.08.22.py
@@ -0,0 +1,40 @@
+
+                # Задание первое. Первый вариант:
+
+# print('Your name,surname and age:')
+#
+# user_name = input()
+# user_surname = input()
+# user_age = input()
+# print(f' Hello! Your name is: {user_name:}. Your surname is: {user_surname:}. Your age: {user_age:}')
+
+                # Задание первое. Второй вариант:
+
+# name = input('Please enter your name:\n')
+# surname = input('Please enter your surname:\n')
+# age = input('Please enter your age:\n')
+# print(f'Hello! Your name is: {name}  {surname} , you age is {age} years old. Thank you!')
+
+                # Второе задание:
+
+
+from math import sqrt
+leg_a = float(input("Enter leg A: "))
+leg_b = float(input("Enter leg B: "))
+print("Hypotenuse = sqrt(a * a + b * b)")
+print("The length of the hypotenuse is c")
+
+print(str("Your answer is:"))
+c = sqrt(leg_a**2 + leg_b**2)
+print(c)
+
+
+                 # Задание четвертое:
+
+# Numbers = input('Enter your numbers or list:\n')
+# print(*Numbers[::-1])
+
+
+
+
+
