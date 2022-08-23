8278717666baa6b4b44dbabca8892431f63a949d
MashaChe1 <maria.chebotareva@gmail.com>
2022-08-23 13:25:37 +0300

    Vitali homework Hero

---
+++ 26.07.22_Home/26.07.22_Lesson_3/Homework_lesson_3.py
@@ -0,0 +1,19 @@
+# Write a code to return "Hero" from given string
+example_string1 = "Hello bro"
+
+print(example_string1[0:2] + example_string1[-2:])
+
+# Write a code to return "Jack is my name"
+example_string2 = "jack Is My NAME"
+
+print(example_string2 .capitalize())
+
+# Write a code to return "Get rid of stars please"
+example_string3 = "*Get rid of stars please*"
+
+print(example_string3 .strip('*'))
+
+# Write a code to return "Hello my name is Jack"
+example_string4 = "hello my name is jack"
+
+print(example_string4 .replace('j', 'J') .replace('h', 'H'))
